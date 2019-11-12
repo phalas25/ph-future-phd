0329cac8f140e0a44b635261da57aa5957004c0c
phalas25 <57474302+phalas25@users.noreply.github.com>
2019-11-11 23:56:31 -0800

    qPCR code

--- qPCR code-Atwood Lab.R
+++ qPCR code-Atwood Lab.R
@@ -14,22 +14,63 @@ library(sqldf)
 #import and rename data set. Remember it needs to be in Git working directory! 
 qPCR_raw <- read.csv ("11_5_19_qPCR_raw_1.csv", header = TRUE)
 colnames(qPCR_raw)
+
 #rename column names
-names(qPCR_raw) <- c("Well", "Sample.Name", "Target.Name", "Task", "Reporter", "Quencher", "RQ", "RQ Min", "RQ Max", "Cт")
+colnames(qPCR_raw) <- c("Well", "Sample.Name", "Target.Name", "Task", "Reporter", "Quencher", "RQ", "RQ Min", "RQ Max", "Cт")
 colnames(qPCR_raw)
-#qPCR_trim <- select(qPCR_raw, 'Sample.Name', 'Target.Name', 'Cт')
+
 #cut out the first 6 rows and the last 5 rows that have no data
 qPCR_c = qPCR_raw [7: nrow(qPCR_raw),]
+head(qPCR_c)
 qPCR_c1 = qPCR_c [1: (nrow(qPCR_c)-5),]
+
 #make a new table with just Sample Name, Target Name and CT 
-qPCR_cut <- select(qPCR_c1, 'Sample.Name', 'Target.Name', 'Cт')
+colnames(qPCR_c1)
+qPCR_c2 = qPCR_c1 [, 1:10]
+qPCR_cut <- select(qPCR_c2, "Sample.Name", "Target.Name", "Cт") 
+qPCR_cut$Cт <- as.numeric(as.character(qPCR_cut$Cт))
+qPCR_cut$Cт
 
+#filter by gli and GAPDH
+qPCR_cut %>% filter(Target.Name == "Gli1") -> qPCR_Gli1
+qPCR_cut %>% filter(Target.Name == "GAPDH")-> qPCR_Gapdh
 
+#Take avg of CT for each group
+qPCR_Gli1 %>% 
+  group_by(Sample.Name,Target.Name) %>% 
+  summarise(avgCт = mean(Cт)) -> Gli
 
+#Take avg of CT for each group
+qPCR_Gapdh %>% 
+  group_by(Sample.Name,Target.Name) %>% 
+  summarise(avgCт = mean(Cт)) -> GAPDH
+                                                                                                       
+#Merge Gli and GAPDH back together
+Merged = merge(Gli,GAPDH, by = "Sample.Name" )
 
+#Create DeltaCT
+Merged['DeltaCT'] = Merged$avgCт.x - Merged$avgCт.y 
 
- 
-  
+#Change Sample.Name column to string
+Merged$Sample.Name <- as.character(Merged$Sample.Name)
+
+#Filter based on 3T3 and not 3T3
+Merged %>% filter(grepl('3T3', Sample.Name)) -> wt
+Merged %>% filter(grepl('BCC', Sample.Name)) -> bcc
+
+#Get 0 m value to subtract from all samples of bcc
+subtract_me = bcc[1,6]
+bcc['DeltaDeltaCT'] = bcc$DeltaCT - subtract_me
+
+#Get 0 m value to subtract from all samples of wt
+subtract_me = wt[1,6]
+wt['DeltaDeltaCT'] = wt$DeltaCT - subtract_me
+
+#Stack wt and bcc on top of each other
+Stacked = rbind(wt,bcc)
+
+#Create fold change column with calculation based on DeltaDeltaCT
+Stacked['Fold Change'] = 2**(-1*Stacked$DeltaDeltaCT) 
 
 
 
