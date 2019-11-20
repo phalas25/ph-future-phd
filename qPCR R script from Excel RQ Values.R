83de4ff0a1043bdfc19e91ff307de220d2960cf5
phalas25 <57474302+phalas25@users.noreply.github.com>
2019-11-19 17:28:50 -0800

    New script with edits

---
+++ qPCR R script from Excel RQ Values.R
@@ -0,0 +1,59 @@
+rm(list = ls(all.names = TRUE))
+library(ggplot2) 
+#Import Excel data and then rename the file to something simpler
+qPCR_DCAA <- X11_18_DCAA
+#Check that the file is correct
+print(qPCR_DCAA)
+
+#Generate Basic Plot
+Plot <- ggplot() + geom_col(data = qPCR_DCAA, aes(x = qPCR_DCAA$`Sample Name`, y = qPCR_DCAA$`Average RQ`, fill = qPCR_DCAA$`Sample Name`))
+print(Plot)
+
+#Add titles to axis as well as graph 
+Plot <- Plot + ggtitle("GLI1 Expression") +
+  xlab("Sample Name") + ylab("Fold Change") + theme_classic() + labs(fill = "Sample Name") + theme(axis.text.x = element_text(size=10, angle=90),axis.title=element_text(size=12,face="bold")) 
+print(Plot)
+
+#Add the RQ Max and Min to the graph
+Plot_SD <- Plot + geom_errorbar(data = qPCR_DCAA, mapping=aes(x=qPCR_DCAA$`Sample Name`, ymin=qPCR_DCAA$`RQ Min`, ymax=qPCR_DCAA$`RQ Max`), width=0.2, size=0.5, color="black")
+print(Plot_SD)
+
+#Rearrange sample names if necessary
+qPCR_DCAA_2 <- select(qPCR_DCAA, "Sample Name", "Average RQ") 
+qPCR_DCAA_2$`Sample Name` <- factor(qPCR_DCAA$`Sample Name`, levels = c("3T3 SS 0 mM DCAA", "3T3 SS 80 mM  DCAA", "3T3 SS 100 mM DCAA", "3T3 SS 126 mM DCAA", "3T3 SS 160 mM DCAA", "3T3 SSH 0 mM DCAA", "3T3 SSH 80 mM DCAA", "3T3 SSH 100 mM DCAA", "3T3 SSH 126 mM DCAA", "3T3 SSH 160 mM DCAA", "BCC SS 0 mM DCAA", "BCC SS 80 mM DCAA", "BCC SS 100 mM DCAA", "BCC SS 126 mM DCAA", "BCC SS 160 mM DCAA"))
+print(qPCR_DCAA_2)
+
+#Generate Basic Plot with rearrange
+Plot <- ggplot() + geom_col(data = qPCR_DCAA_2, aes(x = qPCR_DCAA_2$`Sample Name`, y = qPCR_DCAA_2$`Average RQ`, fill = qPCR_DCAA_2$`Sample Name`))
+print(Plot)
+
+#Add titles to axis as well as graph (for rearrange)
+Plot <- Plot + ggtitle("GLI1 Expression") +
+  xlab("Sample Name") + ylab("Fold Change") + theme_classic() + labs(fill = "Sample Name") + theme(axis.text.x = element_text(size=10, angle=90),axis.title=element_text(size=12,face="bold")) 
+print(Plot)
+
+#Add the RQ Max and Min to the graph (for rearrange)
+Plot_SD <- Plot + geom_errorbar(data = qPCR_DCAA, mapping=aes(x=qPCR_DCAA$`Sample Name`, ymin=qPCR_DCAA$`RQ Min`, ymax=qPCR_DCAA$`RQ Max`), width=0.2, size=0.5, color="black")
+print(Plot_SD)
+
+#Generate new column in table based on cell type and color by it 
+qPCR_DCAA_2$CellType <- NA
+print(qPCR_DCAA_2)
+qPCR_DCAA_2$CellType <- as.numeric(grepl("3T3", qPCR_DCAA_2$`Sample Name`))
+print(qPCR_DCAA_2)
+
+#Generate Basic Plot with rearrange
+library(RColorBrewer)
+Plot <- ggplot() + geom_col(data = qPCR_DCAA_2, aes(x = qPCR_DCAA_2$`Sample Name`, y = qPCR_DCAA_2$`Average RQ`, fill = qPCR_DCAA_2$CellType))
+print(Plot)
+
+#Add titles to axis as well as graph as well as remove legend(for rearrange and color)
+Plot <- Plot + ggtitle("GLI1 Expression") +
+  xlab("Sample Name") + ylab("Fold Change") + theme_classic() + theme(axis.text.x = element_text(size=10, angle=90),axis.title=element_text(size=12,face="bold"), legend.position = "none")
+print(Plot)
+
+#Add the RQ Max and Min to the graph (for rearrange and color)
+Plot_SD <- Plot + geom_errorbar(data = qPCR_DCAA, mapping=aes(x=qPCR_DCAA$`Sample Name`, ymin=qPCR_DCAA$`RQ Min`, ymax=qPCR_DCAA$`RQ Max`), width=0.2, size=0.5, color="black")
+print(Plot_SD)
+
+
